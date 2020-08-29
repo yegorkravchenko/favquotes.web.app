96de2cd9ea1a455401a222d2a41d22bbb05127fa
yegorkravchenko <kraver.kravchenko@gmail.com>
2020-08-29 14:51:21 +0200

    Add random quote displaying on page load

--- app/app.js
+++ app/app.js
@@ -4,21 +4,48 @@ const quoteText = document.querySelector('blockquote > h1');
 const author = document.querySelector('blockquote > p');
 const langBtns = document.querySelectorAll('#lang-change > li');
 
+if (sessionStorage.getItem('language') === null) {
+    sessionStorage.setItem('language', 'ita')
+}
+
+const randomQuoteIndex = quotes[randomInt(0, quotes.length - 1)];
+
 function randomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function randomQuote() {
-    const randomQuoteIndex = quotes[randomInt(0, quotes.length - 1)];
-    quoteText.innerHTML = randomQuoteIndex.ita;
+function setQuote() {
+    switch (sessionStorage.getItem('language')) {
+        case 'ita':
+            quoteText.innerHTML = randomQuoteIndex.ita;
+            author.textContent = randomQuoteIndex.authorLat;
+            break;
+        case 'eng':
+            quoteText.innerHTML = randomQuoteIndex.eng;
+            author.textContent = randomQuoteIndex.authorLat;
+            break;
+        case 'rus':
+            quoteText.innerHTML = randomQuoteIndex.rus;
+            author.textContent = randomQuoteIndex.authorRus;
+            break;
+    }
 }
 
-window.addEventListener('load', randomQuote);
-
 langBtns.forEach(btn => btn.addEventListener('click', () => {
+    let activeLang = Object.values(langBtns).filter(item => item.classList.contains('active'))[0];
+    activeLang.classList.remove('active');
+    
     btn.classList.add('active');
+    sessionStorage.setItem('language', btn.textContent.toLowerCase());
+
+    setQuote();
 }));
 
-console.log(Object.entries(langBtns));
+window.addEventListener('load', () => {
+    setQuote();
+
+    let activeLang = Object.values(langBtns).filter(item => item.textContent.toLowerCase() === sessionStorage.getItem('language'))[0];
+    activeLang.classList.add('active');
+});
