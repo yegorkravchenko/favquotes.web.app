4b5a4dcf181568f98b33c95d2052a037a51e939e
yegorkravchenko <kraver.kravchenko@gmail.com>
2020-08-29 15:46:19 +0200

    Animate reload icon on-click and on-load

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import quotes from './quotes.js';
 const quoteText = document.querySelector('blockquote > h1');
 const author = document.querySelector('blockquote > p');
 const langBtns = document.querySelectorAll('#lang-change > li');
+const reloadIcon = document.querySelector('i');
 
 if (sessionStorage.getItem('language') === null) {
     sessionStorage.setItem('language', 'ita')
@@ -33,6 +34,15 @@ function setQuote() {
     }
 }
 
+function reloadIconAnimation() {
+    const rotate360 = [
+        { transform: 'rotate(0deg)'},
+        { transform: 'rotate(360deg)' }
+    ];
+    
+    reloadIcon.animate(rotate360, { duration: 350 });
+}
+
 langBtns.forEach(btn => btn.addEventListener('click', () => {
     let activeLang = Object.values(langBtns).filter(item => item.classList.contains('active'))[0];
     activeLang.classList.remove('active');
@@ -43,8 +53,14 @@ langBtns.forEach(btn => btn.addEventListener('click', () => {
     setQuote();
 }));
 
+reloadIcon.addEventListener('click', () => {
+    reloadIconAnimation();
+    location.reload();
+});
+
 window.addEventListener('load', () => {
     setQuote();
+    reloadIconAnimation();
 
     let activeLang = Object.values(langBtns).filter(item => item.textContent.toLowerCase() === sessionStorage.getItem('language'))[0];
     activeLang.classList.add('active');
