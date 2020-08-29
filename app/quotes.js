495ddacf16109350b700aa14c1db4580069cf89b
yegorkravchenko <kraver.kravchenko@gmail.com>
2020-08-29 12:10:58 +0200

    Add 2 quotes

---
+++ app/quotes.js
@@ -0,0 +1,18 @@
+const quotes = [
+    {
+        ita: '“La <span class="underlined">felicità</span> nelle persone intelligenti è la cosa più rara che io conosca.”',
+        eng: '“<span class="underlined">Happiness</span> in intelligent people is the rarest thing I know.”',
+        rus: '“<span class="underlined">Умные люди</span> чрезвычайно редко бывают счастливы.”',
+        authorLat: '— Ernest Hemingway',
+        authorRus: '— Эрнест Хемингуэй'
+    },
+    {
+        ita: '“La <span class="underlined">atrocita</span> nelle persone stupide è la cosa più rara che io non conosca.”',
+        eng: '“<span class="underlined">Happiness</span> in intelligent people is the rarest thing I know.”',
+        rus: '“<span class="underlined">Умные люди</span> чрезвычайно редко бывают счастливы.”',
+        authorLat: '— Charles Hemingway',
+        authorRus: '— Эрнест Хемингуэй'
+    }
+];
+
+export default quotes;
